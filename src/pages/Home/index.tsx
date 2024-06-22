1f8bf70e3e025642d8353a2636ac2451606e2ea6
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-22 17:31:12 +0400

    ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'preact/hooks';
 import { Card, TextInput } from 'flowbite-react';
-
+import { FaStar } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { GoIssueOpened } from "react-icons/go";
+import { FaCodeFork } from "react-icons/fa6";
 export function Home() {
 	const [repository, setRepository] = useState(null);
 	const url = "https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/main.json";
@@ -20,7 +23,6 @@ export function Home() {
 				console.error('There has been a problem with your fetch operation:', error);
 			});
 	}, []);
-
 	return (
 		<div>
 			<div className="flex justify-center">
@@ -35,7 +37,8 @@ export function Home() {
 						<Card
 							key={index}
 							className="w-72 my-2 hover:scale-110 transition-transform transform-cpu"
-						><img className="w-10 rounded-full" src={item.owner.avatar_url}/>
+						>
+							<img className="w-10 rounded-full" src={item.owner.avatar_url} />
 							<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 								{item.name[0].toUpperCase() + item.name.slice(1, item.name.length)}
 							</h5>
@@ -43,13 +46,14 @@ export function Home() {
 							<p className="font-normal text-gray-700 dark:text-gray-400">
 								{item.description}
 							</p>
+							<p className="flex items-center">
+								<FaStar size={20} color='#cfbc0e' />&nbsp;{item.stargazers_count}&nbsp;&nbsp;&nbsp;&nbsp; <FaEye color="skyblue" />&nbsp;{item.watchers_count}&nbsp;&nbsp;&nbsp;&nbsp;<FaCodeFork color="lightpink" />&nbsp;{item.forks_count}&nbsp;&nbsp;&nbsp;&nbsp;<GoIssueOpened color="lightgreen" />&nbsp;{item.open_issues}</p>
 						</Card>
 					))
 				) : (
 					<p>Loading...</p>
 				)}
 			</div>
-
 		</div>
 	);
 }
