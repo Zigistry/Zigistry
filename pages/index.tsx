02dd0c352c73cceac29f996c5b95bb7df70fa7b0
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-24 13:56:19 +0400

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,37 +8,16 @@ import { IoMdFastforward } from "react-icons/io";
 
 
 export default function Home({ most_used, top10LatestRepos }: any) {
-	const [most_used_repo, set_most_used_repo] = useState(most_used);
-	const [recent_repo, set_recent_repo] = useState(top10LatestRepos);
-	const [unchangeable, setUnchangeable] = useState(most_used);
-
-	const handleSearchChange = (event: any) => {
-		if (event.target.value == "") {
-			set_most_used_repo(unchangeable);
-		}
-		else {
-			if (unchangeable) {
-				const searchTerm = event.target.value.toLowerCase();
-				const filteredList = unchangeable.filter((item: { full_name: string; description: string; }) =>
-					item.full_name.toLowerCase().includes(searchTerm) ||
-					(item.description && item.description.toLowerCase().includes(searchTerm))
-				);
-
-				set_most_used_repo(filteredList);
-			}
-		}
-	};
-
 	return (
 		<div>
 			<div className='flex flex-col items-center'>
 				<h1 className="text-center font-semibold text-2xl my-5">Search Ziglang Packages</h1>
-				<TextInput onInput={handleSearchChange} placeholder="Search libraries" id="base" type="text" sizing="md" className="w-72 mb-5" />
+				<TextInput placeholder="Search libraries" id="base" type="text" sizing="md" className="w-72 mb-5" />
 			</div>
 			<h1 className="text-left font-semibold text-2xl my-5 ml-10 w-fit border-2 border-slate-400 flex items-center p-4 rounded"><IoMdFastforward size={25} />&nbsp;Recently released:</h1>
 			<div className="w-full flex flex-wrap justify-evenly">
-				{most_used_repo ? (
-					most_used_repo.map((item: any, index: any) => (
+				{top10LatestRepos ? (
+					top10LatestRepos.map((item: any, index: any) => (
 						<Card key={index} className="w-72 my-2 hover:scale-110 transition-transform transform-cpu">
 							<Image width="50" height="50" className="w-10 rounded-full" src={item.owner.avatar_url} alt={item.name} />
 							<h5 className="text-2xl font-bold text-gray-900 dark:text-white">{item.name}</h5>
@@ -91,24 +70,22 @@ interface Repo {
 	[key: string]: any; 
   }
   
-  interface Data {
-	items: Repo[];
-  }
   
   export async function getServerSideProps() {
 	try {
 	  const response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/main.json");
 	  if (!response.ok) throw new Error(`Error: ${response.statusText}`);
-	  
-	  const data: Data = await response.json();
+	  const ori = await response.json();
+
+	  // Create a deep copy of ori to ensure data is a separate object
+	  const data = JSON.parse(JSON.stringify(ori));
   
 	  const sortedRepos = data.items.sort((a: Repo, b: Repo) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
-	  const top10LatestRepos = sortedRepos.slice(0, 10);
   
 	  return {
 		props: {
-		  most_used: data.items.slice(0, 20),
-		  top10LatestRepos,
+		  most_used: ori.items.slice(0, 20),
+		  top10LatestRepos: sortedRepos.slice(0, 10),
 		},
 	  };
 	} catch (error) {
@@ -120,5 +97,4 @@ interface Repo {
 		},
 	  };
 	}
-  }
-  
+}
