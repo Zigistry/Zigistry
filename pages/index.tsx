eeaf549f8e090dd73e035d4aa890c3cb91fad0e8
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-26 17:14:31 +0400

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,23 @@
 import { Button, Card, TextInput } from "flowbite-react";
 import { FaStar, FaEye } from "react-icons/fa";
 import { GoIssueOpened } from "react-icons/go";
-import { useState } from "react";
+import { FocusEventHandler, useState } from "react";
 import { FaCodeFork } from "react-icons/fa6";
 import Image from "next/image";
 import { IoMdFastforward } from "react-icons/io";
 import Link from "next/link";
 import Repo from "@/types/custom_types";
+import { useRouter } from 'next/router';
+
 
 export default function Home(props: {
   most_used: Repo[];
   top10LatestRepos: Repo[];
 }): JSX.Element {
-  const [data, setData] = useState(props.top10LatestRepos);
-  const [show_default, set_show_default] = useState(true);
-  const [inputValue, setInputValue] = useState<string>("");
-
-  const fetchData = async () => {
-    const response = await fetch("/api/search?q=" + inputValue);
-    const result: Repo[] = await response.json();
-    setData(result);
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      fetchData(); // We have now got the result
-      set_show_default(false);
-    }
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
+  const Router = useRouter();
+  const change_route = () => {
+    Router.push("/search");
+  }
   return (
     <div>
       <div className="flex flex-col items-center">
@@ -40,8 +25,7 @@ export default function Home(props: {
           Search Ziglang Packages
         </h1>
         <TextInput
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
+          onFocus={change_route}
           placeholder="Search libraries"
           id="base"
           type="text"
@@ -49,152 +33,102 @@ export default function Home(props: {
           className="w-72 mb-5"
         />
       </div>
-      {show_default ? (
-        <>
-          <h1 className="text-left font-semibold text-2xl my-5 ml-10 w-fit border-2 border-slate-400 flex items-center p-4 rounded">
-            <IoMdFastforward size={25} />
-            &nbsp;Recently released:
-          </h1>
-          <section className="w-full flex flex-wrap justify-evenly">
-            {props.top10LatestRepos ? (
-              props.top10LatestRepos.map((item: Repo, index: number) => (
-                <Card
-                  key={index}
-                  className="w-72 my-2 hover:scale-110 transition-transform transform-cpu"
-                >
-                  <Image
-                    width="50"
-                    height="50"
-                    className="w-10 rounded-full"
-                    src={item.owner.avatar_url}
-                    alt={item.name}
-                  />
-                  <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
-                    {item.name}
-                  </h5>
-                  <p className="text-gray-400">{item.full_name}</p>
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {item.description}
-                  </p>
-                  <div className="flex items-center">
-                    <FaStar size={20} color="#cfbc0e" />
-                    &nbsp;{item.stargazers_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaEye color="skyblue" />
-                    &nbsp;{item.watchers_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaCodeFork color="lightpink" />
-                    &nbsp;{item.forks_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <GoIssueOpened color="lightgreen" />
-                    &nbsp;{item.open_issues}
-                  </div>
-                  <Button as={Link} href={item.full_name} color="dark" pill>
-                    View package
-                  </Button>
-                </Card>
-              ))
-            ) : (
-              <p>Loading...</p>
-            )}
-          </section>
-          <h1 className="text-left font-semibold text-2xl my-5 ml-10 w-fit border-2 border-slate-400 flex items-center p-4 rounded">
-            <FaStar size={25} />
-            &nbsp;Most used:
-          </h1>
-          <section className="w-full flex flex-wrap justify-evenly">
-            {props.most_used ? (
-              props.most_used.map((item: Repo, index: number) => (
-                <Card
-                  key={index}
-                  className="w-72 my-2 hover:scale-110 transition-transform transform-cpu"
-                >
-                  <Image
-                    width="50"
-                    height="50"
-                    className="w-10 rounded-full"
-                    src={item.owner.avatar_url}
-                    alt={item.name}
-                  />
-                  <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
-                    {item.name}
-                  </h5>
-                  <p className="text-gray-400">{item.full_name}</p>
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {item.description}
-                  </p>
-                  <div className="flex items-center">
-                    <FaStar size={20} color="#cfbc0e" />
-                    &nbsp;{item.stargazers_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaEye color="skyblue" />
-                    &nbsp;{item.watchers_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaCodeFork color="lightpink" />
-                    &nbsp;{item.forks_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <GoIssueOpened color="lightgreen" />
-                    &nbsp;{item.open_issues}
-                  </div>
-                  <Button as={Link} href={item.full_name} color="dark" pill>
-                    View package
-                  </Button>
-                </Card>
-              ))
-            ) : (
-              <p>Loading...</p>
-            )}
-          </section>
-        </>
-      ) : (
-        <>
-          <section className="w-full flex flex-wrap justify-evenly">
-            {data ? (
-              data.map((item: Repo, index: number) => (
-                <Card
-                  key={index}
-                  className="w-72 my-2 hover:scale-110 transition-transform transform-cpu"
-                >
-                  <Image
-                    width="50"
-                    height="50"
-                    className="w-10 rounded-full"
-                    src={item.owner.avatar_url}
-                    alt={item.name}
-                  />
-                  <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
-                    {item.name}
-                  </h5>
-                  <p className="text-gray-400">{item.full_name}</p>
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {item.description}
-                  </p>
-                  <div className="flex items-center">
-                    <FaStar size={20} color="#cfbc0e" />
-                    &nbsp;{item.stargazers_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaEye color="skyblue" />
-                    &nbsp;{item.watchers_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <FaCodeFork color="lightpink" />
-                    &nbsp;{item.forks_count}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <GoIssueOpened color="lightgreen" />
-                    &nbsp;{item.open_issues}
-                  </div>
-                  <Button as={Link} href={item.full_name} color="dark" pill>
-                    View package
-                  </Button>
-                </Card>
-              ))
-            ) : (
-              <p>Loading...</p>
-            )}
-          </section>
-        </>
-      )}
+      <h1 className="text-left font-semibold text-2xl my-5 ml-10 w-fit border-2 border-slate-400 flex items-center p-4 rounded">
+        <IoMdFastforward size={25} />
+        &nbsp;Recently released:
+      </h1>
+      <section className="w-full flex flex-wrap justify-evenly">
+        {props.top10LatestRepos ? (
+          props.top10LatestRepos.map((item: Repo, index: number) => (
+            <Card
+              key={index}
+              className="w-72 my-2 hover:scale-110 transition-transform transform-cpu"
+            >
+              <Image
+                width="50"
+                height="50"
+                className="w-10 rounded-full"
+                src={item.owner.avatar_url}
+                alt={item.name}
+              />
+              <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
+                {item.name}
+              </h5>
+              <p className="text-gray-400">{item.full_name}</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                {item.description}
+              </p>
+              <div className="flex items-center">
+                <FaStar size={20} color="#cfbc0e" />
+                &nbsp;{item.stargazers_count}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <FaEye color="skyblue" />
+                &nbsp;{item.watchers_count}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <FaCodeFork color="lightpink" />
+                &nbsp;{item.forks_count}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <GoIssueOpened color="lightgreen" />
+                &nbsp;{item.open_issues}
+              </div>
+              <Button as={Link} href={item.full_name} color="dark" pill>
+                View package
+              </Button>
+            </Card>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
+      <h1 className="text-left font-semibold text-2xl my-5 ml-10 w-fit border-2 border-slate-400 flex items-center p-4 rounded">
+        <FaStar size={25} />
+        &nbsp;Most used:
+      </h1>
+      <section className="w-full flex flex-wrap justify-evenly">
+        {props.most_used ? (
+          props.most_used.map((item: Repo, index: number) => (
+            <Card
+              key={index}
+              className="w-72 my-2 hover:scale-110 transition-transform transform-cpu"
+            >
+              <Image
+                width="50"
+                height="50"
+                className="w-10 rounded-full"
+                src={item.owner.avatar_url}
+                alt={item.name}
+              />
+              <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
+                {item.name}
+              </h5>
+              <p className="text-gray-400">{item.full_name}</p>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                {item.description}
+              </p>
+              <div className="flex items-center">
+                <FaStar size={20} color="#cfbc0e" />
+                &nbsp;{item.stargazers_count}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <FaEye color="skyblue" />
+                &nbsp;{item.watchers_count}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <FaCodeFork color="lightpink" />
+                &nbsp;{item.forks_count}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <GoIssueOpened color="lightgreen" />
+                &nbsp;{item.open_issues}
+              </div>
+              <Button as={Link} href={item.full_name} color="dark" pill>
+                View package
+              </Button>
+            </Card>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
     </div>
-  );
+  )
 }
 
 export async function getServerSideProps(): Promise<{
@@ -269,7 +203,6 @@ export async function getServerSideProps(): Promise<{
           open_issues,
         }),
       );
-
     return {
       props: {
         most_used,
