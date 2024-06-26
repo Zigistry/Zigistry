4a51343051b09d24903879b5a6cf1f4c80b871f1
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-26 19:30:52 +0400

    ok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { JSX } from "react"
-import Layout from "./components/layout"
+import Layout from "../components/layout"
 import './globals.css'
 
 export default function MyApp(Props: { Component: JSX.ElementType, pageProps: JSX.IntrinsicAttributes }): JSX.Element {
