a2521a74546afb7cf8ee544c5ebf1ba351ceb7d8
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-28 16:14:09 +0400

    ok

--- scripts/runner.sh
+++
@@ -1,8 +0,0 @@
-brew install zig
-curl -o ./a.json https://example.com/data
-curl -o ./b.json https://example.com/data
-curl -o ./c.json https://example.com/data
-zig run ./scripts/main.zig > ./database/main.json
-rm -rf ./a.json
-rm -rf ./b.json
-rm -rf ./c.json
