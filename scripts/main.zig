92b5ef70e1e43f502ecebfff8d7c0f64c176981d
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-28 16:54:41 +0400

    ok

--- scripts/main.zig
+++ scripts/main.zig
@@ -1,34 +1,35 @@
 const std = @import("std");
-const print = std.debug.print;
+const writer = std.io.getStdOut().writer();
+
 
 fn print_repos(my_items: []std.json.Value) !void {
     for (my_items, 0..) |item, i| {
-        print("\n{{\n", .{});
-        print("  \"name\": \"{s}\",\n", .{item.object.get("name").?.string});
-        print("  \"full_name\" : \"{s}\",\n", .{item.object.get("full_name").?.string});
+        try writer.print("\n{{\n", .{});
+        try writer.print("  \"name\": \"{s}\",\n", .{item.object.get("name").?.string});
+        try writer.print("  \"full_name\" : \"{s}\",\n", .{item.object.get("full_name").?.string});
         if (item.object.get("description").? == .string) {
-            print("  \"description\":\"{s}\",\n", .{item.object.get("description").?.string});
+            try writer.print("  \"description\":\"{s}\",\n", .{item.object.get("description").?.string});
         } else {
-            print("  \"description\":\"{s}\",\n", .{"This repo has no desciption."});
+            try writer.print("  \"description\":\"{s}\",\n", .{"This repo has no desciption."});
         }
-        print("  \"watchers_count\" :{},\n", .{item.object.get("watchers_count").?.integer});
-        print("  \"forks_count\":{},\n", .{item.object.get("forks_count").?.integer});
-        print("  \"open_issues\":{},\n", .{item.object.get("open_issues").?.integer});
-        print("  \"stargazers_count\":{},\n", .{item.object.get("stargazers_count").?.integer});
-        print("  \"tags_url\",\"{s}\",\n", .{item.object.get("tags_url").?.string});
-        print("  \"owner\":{{\n    \"avatar_url\": \"{s}\" \n  }}\n", .{item.object.get("owner").?.object.get("avatar_url").?.string});
-        print("  \"created_at\": \"{s}\"\n", .{item.object.get("created_at").?.string});
+        try writer.print("  \"watchers_count\" :{},\n", .{item.object.get("watchers_count").?.integer});
+        try writer.print("  \"forks_count\":{},\n", .{item.object.get("forks_count").?.integer});
+        try writer.print("  \"open_issues\":{},\n", .{item.object.get("open_issues").?.integer});
+        try writer.print("  \"stargazers_count\":{},\n", .{item.object.get("stargazers_count").?.integer});
+        try writer.print("  \"tags_url\",\"{s}\",\n", .{item.object.get("tags_url").?.string});
+        try writer.print("  \"owner\":{{\n    \"avatar_url\": \"{s}\" \n  }}\n", .{item.object.get("owner").?.object.get("avatar_url").?.string});
+        try writer.print("  \"created_at\": \"{s}\"\n", .{item.object.get("created_at").?.string});
         if (i == my_items.len - 1) {
-            print("}}\n", .{});
+            try writer.print("}}\n", .{});
         } else {
-            print("}},\n", .{});
+            try writer.print("}},\n", .{});
         }
     }
 }
 
 pub fn main() !void {
     const file_names = [3][]const u8{ "a.json", "b.json", "c.json" };
-    print("[", .{});
+    try writer.print("[", .{});
     for (file_names) |file_name| {
         const file = try std.fs.cwd().openFile(file_name, .{});
         const buf_alloc = std.heap.page_allocator;
@@ -44,5 +45,5 @@ pub fn main() !void {
         const my_items = parsed.value.object.get("items").?.array.items;
         try print_repos(my_items);
     }
-    print("]", .{});
+    try writer.print("]", .{});
 }
