9a04941b5d229f0257da8dc478ef733fbb269540
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-28 20:01:46 +0400

    ok

--- scripts/main.zig
+++ scripts/main.zig
@@ -1,13 +1,26 @@
 const std = @import("std");
 const writer = std.io.getStdOut().writer();
 
+fn replace(allocator: std.mem.Allocator, str:[]const u8, char_to_replace:u8, replace_with:u8) ![]const u8 {
+    var my_custom = std.ArrayList(u8).init(allocator);
+    for(str)|char|{
+        if (char == char_to_replace) {
+            try my_custom.append(replace_with);
+        }
+        else{
+            try my_custom.append(char);
+        }
+    }
+    return my_custom.toOwnedSlice();
+}
+
 fn print_repos(my_items: []std.json.Value, is_last_file: bool) !void {
     for (my_items, 0..) |item, i| {
         try writer.print("\n{{\n", .{});
         try writer.print("  \"name\": \"{s}\",\n", .{item.object.get("name").?.string});
         try writer.print("  \"full_name\" : \"{s}\",\n", .{item.object.get("full_name").?.string});
         if (item.object.get("description").? == .string) {
-            try writer.print("  \"description\":\"{s}\",\n", .{item.object.get("description").?.string});
+            try writer.print("  \"description\":\"{s}\",\n", .{try replace(std.heap.page_allocator,item.object.get("description").?.string, '"', '\'')});
         } else {
             try writer.print("  \"description\":\"{s}\",\n", .{"This repo has no desciption."});
         }
