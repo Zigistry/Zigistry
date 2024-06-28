7b0f058d5a6f0a43e027742e2e23addba4cfac67
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-28 17:34:37 +0400

    ok

--- scripts/main.zig
+++ scripts/main.zig
@@ -9,7 +9,7 @@ fn print_repos(my_items: []std.json.Value, is_last_file: bool) !void {
         if (item.object.get("description").? == .string) {
             try writer.print("  \"description\":\"{s}\",\n", .{item.object.get("description").?.string});
         } else {
-            try writer.print("  \"description\":\"{s}\",\n", .{"This repo has no desciption."});
+            try writer.print("  \"description\":\"{s}\",\n", .{"This repo has no description."});
         }
         try writer.print("  \"watchers_count\" :{},\n", .{item.object.get("watchers_count").?.integer});
         try writer.print("  \"forks_count\":{},\n", .{item.object.get("forks_count").?.integer});
@@ -30,6 +30,7 @@ pub fn main() !void {
     const file_names = [3][]const u8{ "a.json", "b.json", "c.json" };
     try writer.print("[", .{});
     for (file_names, 0..) |file_name, i| {
+        try writer.print("Processing file: {s}\n", .{file_name});
         const file = try std.fs.cwd().openFile(file_name, .{});
         const buf_alloc = std.heap.page_allocator;
         const buf = try file.readToEndAlloc(buf_alloc, try file.getEndPos());
@@ -41,6 +42,12 @@ pub fn main() !void {
             .{},
         );
         defer parsed.deinit();
+        
+        if (parsed.value.object.get("items") == null) {
+            try writer.print("Error: 'items' key not found in file: {s}\n", .{file_name});
+            continue;
+        }
+        
         const my_items = parsed.value.object.get("items").?.array.items;
         if (i == file_names.len - 1) {
             try print_repos(my_items, true);
