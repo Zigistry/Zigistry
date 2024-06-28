c32b52fbaac775145cba2f2150feada38b9c13e5
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-29 02:15:29 +0400

    ok

--- scripts/main.zig
+++ scripts/main.zig
@@ -24,10 +24,10 @@ fn print_json(x: []const u8, y: []const u8, end_with_comma: bool) void {
     } else print("\"{s}\":\"{s}\"\n", .{ x, y });
 }
 
-fn print_json_int(x: []const u8, y: i8, end_with_comma: bool) void {
+fn print_json_int(x: []const u8, y: i64, end_with_comma: bool) void {
     if (end_with_comma) {
-        print("\"{s}\" : \"{s}\",\n", .{ x, y });
-    } else print("\"{s}\" : \"{s}\"\n", .{ x, y });
+        print("\"{s}\" : \"{}\",\n", .{ x, y });
+    } else print("\"{s}\" : \"{}\"\n", .{ x, y });
 }
 
 fn print_repos(my_items: []std.json.Value, is_last_file: bool) !void {
