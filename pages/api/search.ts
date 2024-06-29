4579c341cc8bc284c7bc8be213e4b06ae7166157
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-29 22:23:03 +0400

    ok

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -11,7 +11,7 @@ export default async function handler(
     const response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/main.json");
     if (!response.ok) throw new Error(`Error: ${response.statusText}`);
     const items: Repo[] = await response.json();
-    return res.status(200).json(items.filter(item => item.name.includes(q.toString())));
+    return res.status(200).json(items.filter(item => item.name.toLowerCase().includes(q.toString().toLowerCase())));
   }
   return res.status(200).json([]);
 }
