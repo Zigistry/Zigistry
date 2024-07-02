609a90e578cb7bb8f1dbd6cc6ef290a0a4082c88
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-07-03 03:53:01 +0400

    ok

--- backend/recommendations_generator.ts
+++ backend/recommendations_generator.ts
@@ -28,15 +28,15 @@ export default async function recommendation_backend() {
   if (!response.ok) throw new Error(`Error: ${response.statusText}`);
   const items: Repo[] = await response.json();
 
-  const gaming_response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/games.json");
+  const gaming_response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/out/games.json");
   if (!gaming_response.ok) throw new Error(`Error: ${gaming_response.statusText}`);
   const gaming_items: Repo[] = await gaming_response.json();
 
-  const web_response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/web.json");
+  const web_response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/out/web.json");
   if (!web_response.ok) throw new Error(`Error: ${web_response.statusText}`);
   const web_items: Repo[] = await web_response.json();
 
-  const gui_response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/gui.json");
+  const gui_response = await fetch("https://raw.githubusercontent.com/RohanVashisht1234/zigistry/main/database/out/gui.json");
   if (!gui_response.ok) throw new Error(`Error: ${gui_response.statusText}`);
   const gui_items: Repo[] = await gui_response.json();
 
