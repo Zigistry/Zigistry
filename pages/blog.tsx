e2c7fa7f45dd54a3632cecd2c5171fadf61ce500
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-07-03 17:43:19 +0400

    ok

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,6 +3,8 @@ import path from 'path';
 import Link from 'next/link';
 import { useState } from 'react';
 import { TextInput } from 'flowbite-react';
+import { Card } from "flowbite-react";
+
 
 export async function getStaticProps() {
   const blogDirectory = path.join(process.cwd(), 'pages', 'blog');
@@ -41,25 +43,28 @@ function Blog({ posts }: BlogProps) {
   return (
     <ul>
       <div className='flex items-center flex-col'>
-      <h1 className="text-center font-semibold text-2xl my-5">Search Ziglang Blogs</h1>
-      <TextInput
-        onChange={(e) => my_func(e.target.value)}
-        placeholder="Search libraries"
-        className="w-72 mb-5"
-        autoFocus
-      /></div>
-      <div className='flex items-center justify-center'>
-      {my_posts.length > 0 ? (
-        my_posts.map((post) => (
-          <li key={post.slug}>
-            <Link className='bg-slate-400 p-4 m-5 rounded' href={`/blog/${post.slug}`}>
-              {post.title}
-            </Link>
-          </li>
-        ))
-      ) : (
-        <li>No posts found</li>
-      )}
+        <h1 className="text-center font-semibold text-2xl my-5">Search Ziglang Blogs</h1>
+        <TextInput
+          onChange={(e) => my_func(e.target.value)}
+          placeholder="Search libraries"
+          className="w-72 mb-5"
+          autoFocus
+        /></div>
+      <div className='w-full flex flex-wrap justify-evenly'>
+        {my_posts.length > 0 ? (
+          my_posts.map((post, i) => (
+            <Card as={Link} key={i} href={"blog/"+post.slug} className="max-w-sm my-4">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {post.title[0].toUpperCase() + post.title.slice(1)}
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+            </Card>
+          ))
+        ) : (
+          <li>No posts found</li>
+        )}
       </div>
     </ul>
   );
