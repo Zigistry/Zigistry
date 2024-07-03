530af0a3fdf5256697739fc706d533247fca368c
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-07-03 17:47:25 +0400

    ok

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -6,23 +6,6 @@ import { TextInput } from 'flowbite-react';
 import { Card } from "flowbite-react";
 
 
-export async function getStaticProps() {
-  const blogDirectory = path.join(process.cwd(), 'pages', 'blog');
-  const filenames = fs.readdirSync(blogDirectory);
-
-  const posts = filenames.map((filename) => {
-    return {
-      slug: filename.replace(/\.mdx$/, ''),
-      title: filename.replace(/\.mdx$/, '').replace(/-/g, ' '), // Assuming the title is derived from the filename
-    };
-  });
-
-  return {
-    props: {
-      posts,
-    },
-  };
-}
 
 interface Post {
   slug: string;
@@ -33,7 +16,7 @@ interface BlogProps {
   posts: Post[];
 }
 
-function Blog({ posts }: BlogProps) {
+export default function Blog({ posts }: BlogProps) {
   const [my_posts, set_my_posts] = useState(posts);
 
   function my_func(x: string) {
@@ -53,13 +36,15 @@ function Blog({ posts }: BlogProps) {
       <div className='w-full flex flex-wrap justify-evenly'>
         {my_posts.length > 0 ? (
           my_posts.map((post, i) => (
-            <Card as={Link} key={i} href={"blog/"+post.slug} className="max-w-sm my-4">
-              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {post.title[0].toUpperCase() + post.title.slice(1)}
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-              </p>
+            <Card key={i} className="max-w-sm my-4">
+              <Link href={"blog/" + post.slug}>
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {post.title[0].toUpperCase() + post.title.slice(1)}
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                </p>
+              </Link>
             </Card>
           ))
         ) : (
@@ -70,4 +55,20 @@ function Blog({ posts }: BlogProps) {
   );
 }
 
-export default Blog;
+export async function getStaticProps() {
+  const blogDirectory = path.join(process.cwd(), 'pages', 'blog');
+  const filenames = fs.readdirSync(blogDirectory);
+
+  const posts = filenames.map((filename) => {
+    return {
+      slug: filename.replace(/\.mdx$/, ''),
+      title: filename.replace(/\.mdx$/, '').replace(/-/g, ' '), // Assuming the title is derived from the filename
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
