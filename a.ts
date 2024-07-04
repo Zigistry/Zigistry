33ff5c505c791054af1fc7338521d776afbcf9b4
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-07-04 17:24:19 +0400

    ok

--- a.ts
+++
@@ -1,9 +0,0 @@
-if(user_input.includes('how') && user_input.includes('you') && user_input.includes('you')) {
-    return 'I am fine, thanks!';
-}
-if(user_input.includes('what') && user_input.includes('you') && user_input.includes('do')) {
-    return 'I help people find zig lang libraries as per their needs, what are your needs?';
-}
-if(user_input.includes('web') && user_input.includes('web') && user_input.includes('lib')) {
-    return 'I know about some of most famous web libraries made in zig like';
-}
