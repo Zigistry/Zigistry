286c50ee1a58e17d50cdef2a8f4c38620bc3d0c5
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-07-16 01:41:47 +0400

    ok

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -41,9 +41,6 @@ export default function Blog({ posts }: BlogProps) {
                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   {post.title[0].toUpperCase() + post.title.slice(1)}
                 </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                </p>
               </Link>
             </Card>
           ))
