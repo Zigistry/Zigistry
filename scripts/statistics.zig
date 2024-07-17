bebc10540f64e4afca5729f8259475339d73b9f0
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-07-18 00:20:22 +0400

    ok

--- scripts/statistics.zig
+++ scripts/statistics.zig
@@ -22,7 +22,29 @@ pub fn main() !void {
     }
     var as = hashmap.iterator();
     while (as.next()) |entry| {
-        std.debug.print("{d}\n", .{ entry.value_ptr.*});
+        std.debug.print("{s}:{d}\n", .{entry.key_ptr.*,entry.value_ptr.*});
     }
     hashmap.deinit();
+    var mine = std.ArrayList([]const u8).init(std.heap.c_allocator);
+    for (parsed_items) |item| {
+        const items = item.object.get("topics").?.array.items;
+        for(items)|single_item|{
+            try mine.append(single_item.string);
+        }
+    }
+    var hashmapmine = std.StringHashMap(u32).init(std.heap.page_allocator);
+    const as_string:[][]const u8 = try mine.toOwnedSlice();
+    for (as_string) |ok| {
+        const existing_count = hashmapmine.get(ok);
+        if (existing_count) |count| {
+            hashmapmine.put(ok, count + 1) catch {};
+        } else {
+            hashmapmine.put(ok, 1) catch {};
+        }
+    }
+    var asdf = hashmapmine.iterator();
+    while (asdf.next()) |entry| {
+                std.debug.print("{s}:{d}\n", .{entry.key_ptr.*,entry.value_ptr.*});
+
+    }
 }
