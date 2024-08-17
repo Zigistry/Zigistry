e266722850da887e1683afec3fd92af758e3d038
rohanvashisht <rohanprogrammer1@gmail.com>
2024-08-18 03:24:15 +0530

    ok

--- scripts/codeberg.zig
+++ scripts/codeberg.zig
@@ -4,12 +4,10 @@ const hp = @import("helperFunctions");
 pub fn main() !void {
     hp.print("[", .{});
     const res = try hp.fetch(hp.globalAllocator, "https://codeberg.org/api/v1/repos/search?q=zig");
-    if(std.mem.eql(u8, "", res)){
+    if (std.mem.eql(u8, "", res)) {
         @panic("can't connect to codeberg.");
     }
     const parsed = try std.json.parseFromSlice(std.json.Value, hp.globalAllocator, res, .{});
     try hp.compressAndPrintReposBerg(parsed.value.object.get("data").?.array.items, true);
     hp.print("]", .{});
 }
-
-
