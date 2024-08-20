1035830a9004fee3a33d0d6f1aaa7c5b3e4d5b06
rohanvashisht <rohanprogrammer1@gmail.com>
2024-08-21 00:14:44 +0530

    ok

--- components/CustomCard.tsx
+++ components/CustomCard.tsx
@@ -38,7 +38,15 @@ export default function CustomCard(props: { item: Repo }) {
       <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
         {props.item.name}
       </h5>
-      <p className="text-gray-400">{props.item.full_name} <Badge color={"darkblue"} className="w-fit dark:bg-slate-600 bg-white dark:border-none border-slate-200 border mt-4">{props.item.license}</Badge></p>
+      <p className="text-gray-400">
+        {props.item.full_name}
+        <div className="flex space-x-3">
+          {props.item.archived ?<Badge color={"light"} className="w-fit dark:bg-yellow-400 bg-white dark:border-none border-slate-200 border mt-4">Archived</Badge>: ""}
+          <Badge color={"darkblue"} className="w-fit dark:bg-slate-600 bg-white dark:border-none border-slate-200 border mt-4">{props.item.license}</Badge>
+          
+
+        </div>
+      </p>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         {props.item.description}
       </p>
