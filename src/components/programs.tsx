62f9d086caa71c9bc58faec2232e826409c82ff7
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-09-23 10:55:02 +0530

    ok

--- src/components/programs.tsx
+++ src/components/programs.tsx
@@ -33,178 +33,178 @@ import bergdb from "../../database/codebergPrograms.json";
 //       Exports "/programs"
 // =============================
 export default function Programs(props: {
-    mostUsed: Repo[];
-    top10LatestRepos: Repo[];
+  mostUsed: Repo[];
+  top10LatestRepos: Repo[];
 }) {
-    const [infiniteScrollItems, setInfiniteScrollItems] = useState([
-        placeHolderRepoType,
-    ]);
-    const [hasMore, setHasMore] = useState(true);
-    const [infiniteScrollIndex, setIndex] = useState(3);
+  const [infiniteScrollItems, setInfiniteScrollItems] = useState([
+    placeHolderRepoType,
+  ]);
+  const [hasMore, setHasMore] = useState(true);
+  const [infiniteScrollIndex, setIndex] = useState(3);
 
-    useEffect(() => {
-        fetch("/api/infiniteScrollPrograms?pageNumber=2")
-            .then((res) => res.json())
-            .then((data) => setInfiniteScrollItems(data))
-            .catch((err) => console.log(err));
-    }, []);
+  useEffect(() => {
+    fetch("/api/infiniteScrollPrograms?pageNumber=2")
+      .then((res) => res.json())
+      .then((data) => setInfiniteScrollItems(data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    const fetchMoreData = () => {
-        fetch(`/api/infiniteScrollPrograms?pageNumber=${infiniteScrollIndex}`)
-            .then((res) => res.json())
-            .then((data) => {
-                setInfiniteScrollItems((prevItems) => [...prevItems, ...data]);
+  const fetchMoreData = () => {
+    fetch(`/api/infiniteScrollPrograms?pageNumber=${infiniteScrollIndex}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setInfiniteScrollItems((prevItems) => [...prevItems, ...data]);
 
-                data.length > 0 ? setHasMore(true) : setHasMore(false);
-            })
-            .catch((err) => console.log(err));
+        data.length > 0 ? setHasMore(true) : setHasMore(false);
+      })
+      .catch((err) => console.log(err));
 
-        setIndex((prevIndex) => prevIndex + 1);
-    };
-    const [searchResultsData, setSearchResultsData] = useState([
-        placeHolderRepoType,
-    ]);
-    const [showDefaultIndexPage, setShowDefaultIndexPage] = useState(true);
-    const [searchTextboxInputValue, setSearchTextboxInputValue] = useState("");
-    // ------- prevent user ddos --------
-    const [dataInTextboxChanged, setDataInTextboxChanged] = useState(false);
+    setIndex((prevIndex) => prevIndex + 1);
+  };
+  const [searchResultsData, setSearchResultsData] = useState([
+    placeHolderRepoType,
+  ]);
+  const [showDefaultIndexPage, setShowDefaultIndexPage] = useState(true);
+  const [searchTextboxInputValue, setSearchTextboxInputValue] = useState("");
+  // ------- prevent user ddos --------
+  const [dataInTextboxChanged, setDataInTextboxChanged] = useState(false);
 
-    // ----------- Fetch search results -------------
-    const fetchData = async () => {
-        const val: HTMLSelectElement = document.getElementById(
-            "dropDownID",
-        ) as HTMLSelectElement;
-        if (searchTextboxInputValue !== "" && dataInTextboxChanged) {
-            setDataInTextboxChanged(false);
-            let response;
-            if (val.value === "No Filter") {
-                response = await fetch(
-                    "/api/searchPrograms?q=" + searchTextboxInputValue,
-                );
-            } else {
-                response = await fetch(
-                    "/api/searchPrograms?q=" +
-                    searchTextboxInputValue +
-                    "&filter=" +
-                    val.value,
-                );
-            }
-            const result: Repo[] = await response.json();
-            setSearchResultsData(result);
-        }
-    };
-
-    const handleKeyDown = (event: { key: string }) => {
-        if (event.key == "Enter") {
-            fetchData();
-            setShowDefaultIndexPage(false);
-        }
-    };
-    const okf = async () => {
-        const val: HTMLSelectElement = document.getElementById(
-            "dropDownID",
-        ) as HTMLSelectElement;
-        if (val.value == "No Filter") {
-            setShowDefaultIndexPage(true);
-        } else {
-            const response = await fetch("/api/searchPrograms?filter=" + val.value);
-            const result: Repo[] = await response.json();
-            setShowDefaultIndexPage(false);
-            setSearchResultsData(result);
-        }
-    };
-
-    function handleOnChage(z: string) {
-        setDataInTextboxChanged(true);
-        if (z == "") {
-            setShowDefaultIndexPage(true);
-        } else {
-            setSearchTextboxInputValue(z);
-        }
+  // ----------- Fetch search results -------------
+  const fetchData = async () => {
+    const val: HTMLSelectElement = document.getElementById(
+      "dropDownID",
+    ) as HTMLSelectElement;
+    if (searchTextboxInputValue !== "" && dataInTextboxChanged) {
+      setDataInTextboxChanged(false);
+      let response;
+      if (val.value === "No Filter") {
+        response = await fetch(
+          "/api/searchPrograms?q=" + searchTextboxInputValue,
+        );
+      } else {
+        response = await fetch(
+          "/api/searchPrograms?q=" +
+            searchTextboxInputValue +
+            "&filter=" +
+            val.value,
+        );
+      }
+      const result: Repo[] = await response.json();
+      setSearchResultsData(result);
     }
-    return (
-        <>
-            <div className="flex flex-col items-center">
-                <h1 className="my-5 text-center text-2xl font-semibold">
-                    Search Ziglang Programs
-                </h1>
-                <div className="flex">
-                    <Tooltip content="Search by filtering github topics">
-                        <Select onChange={okf} id="dropDownID" required={false}>
-                            <option>No Filter</option>
-                            <option>api</option>
-                            <option>http</option>
-                            <option>rest</option>
-                            <option>gamedev</option>
-                            <option>gui</option>
-                            <option>cross-platform</option>
-                        </Select>
-                    </Tooltip>
-                    <TextInput
-                        onChange={(e) => handleOnChage(e.target.value)}
-                        onKeyUp={handleKeyDown}
-                        id="SearchBox"
-                        placeholder="Search 1000+ Zig programs"
-                        className="mb-5 ml-2 w-72"
-                        autoFocus
-                    />
-                </div>
-            </div>
-            {showDefaultIndexPage ? (
-                <>
-                    <h1 className="my-5 ml-10 flex w-fit items-center rounded border-2 border-slate-400 p-4 text-left text-2xl font-semibold">
-                        <IoMdFastforward size={25} />
-                        &nbsp;Recently released:
-                    </h1>
-                    <section className="flex w-full flex-wrap justify-evenly">
-                        {props.top10LatestRepos.map((item: Repo, index: number) => (
-                            <CustomCard program={true} key={index} item={item} />
-                        ))}
-                    </section>
-                    <h1 className="my-5 ml-10 flex w-fit items-center rounded border-2 border-slate-400 p-4 text-left text-2xl font-semibold">
-                        <FaStar size={25} />
-                        &nbsp;Most used:
-                    </h1>
-                    <section className="flex w-full flex-wrap justify-evenly">
-                        {props.mostUsed.map((item: Repo, index: number) => (
-                            <CustomCard program={true} key={index} item={item} />
-                        ))}
-                    </section>
-                    <h1 className="my-5 ml-10 flex w-fit items-center rounded border-2 border-slate-400 p-4 text-left text-2xl font-semibold">
-                        <SlGlobe size={25} />
-                        &nbsp;View more:
-                    </h1>
+  };
 
-                    <InfiniteScroll
-                        dataLength={infiniteScrollItems.length}
-                        next={fetchMoreData}
-                        hasMore={hasMore}
-                        loader={undefined}
-                    >
-                        <section className="flex w-full flex-wrap justify-evenly">
-                            {infiniteScrollItems ? (
-                                infiniteScrollItems.map((item: Repo, index: number) => (
-                                    <CustomCard program={true} key={index} item={item} />
-                                ))
-                            ) : (
-                                <p>Loading...</p>
-                            )}
-                        </section>
-                    </InfiniteScroll>
-                </>
-            ) : (
-                <section className="flex w-full flex-wrap justify-evenly">
-                    {searchResultsData.length ? (
-                        searchResultsData.map((item, index) => (
-                            <CustomCard program={true} key={index} item={item} />
-                        ))
-                    ) : (
-                        <h1>Can&apos;t find what you are looking for</h1>
-                    )}
-                </section>
-            )}
+  const handleKeyDown = (event: { key: string }) => {
+    if (event.key == "Enter") {
+      fetchData();
+      setShowDefaultIndexPage(false);
+    }
+  };
+  const okf = async () => {
+    const val: HTMLSelectElement = document.getElementById(
+      "dropDownID",
+    ) as HTMLSelectElement;
+    if (val.value == "No Filter") {
+      setShowDefaultIndexPage(true);
+    } else {
+      const response = await fetch("/api/searchPrograms?filter=" + val.value);
+      const result: Repo[] = await response.json();
+      setShowDefaultIndexPage(false);
+      setSearchResultsData(result);
+    }
+  };
+
+  function handleOnChage(z: string) {
+    setDataInTextboxChanged(true);
+    if (z == "") {
+      setShowDefaultIndexPage(true);
+    } else {
+      setSearchTextboxInputValue(z);
+    }
+  }
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <h1 className="my-5 text-center text-2xl font-semibold">
+          Search Ziglang Programs
+        </h1>
+        <div className="flex">
+          <Tooltip content="Search by filtering github topics">
+            <Select onChange={okf} id="dropDownID" required={false}>
+              <option>No Filter</option>
+              <option>api</option>
+              <option>http</option>
+              <option>rest</option>
+              <option>gamedev</option>
+              <option>gui</option>
+              <option>cross-platform</option>
+            </Select>
+          </Tooltip>
+          <TextInput
+            onChange={(e) => handleOnChage(e.target.value)}
+            onKeyUp={handleKeyDown}
+            id="SearchBox"
+            placeholder="Search 1000+ Zig programs"
+            className="mb-5 ml-2 w-72"
+            autoFocus
+          />
+        </div>
+      </div>
+      {showDefaultIndexPage ? (
+        <>
+          <h1 className="my-5 ml-10 flex w-fit items-center rounded border-2 border-slate-400 p-4 text-left text-2xl font-semibold">
+            <IoMdFastforward size={25} />
+            &nbsp;Recently released:
+          </h1>
+          <section className="flex w-full flex-wrap justify-evenly">
+            {props.top10LatestRepos.map((item: Repo, index: number) => (
+              <CustomCard program={true} key={index} item={item} />
+            ))}
+          </section>
+          <h1 className="my-5 ml-10 flex w-fit items-center rounded border-2 border-slate-400 p-4 text-left text-2xl font-semibold">
+            <FaStar size={25} />
+            &nbsp;Most used:
+          </h1>
+          <section className="flex w-full flex-wrap justify-evenly">
+            {props.mostUsed.map((item: Repo, index: number) => (
+              <CustomCard program={true} key={index} item={item} />
+            ))}
+          </section>
+          <h1 className="my-5 ml-10 flex w-fit items-center rounded border-2 border-slate-400 p-4 text-left text-2xl font-semibold">
+            <SlGlobe size={25} />
+            &nbsp;View more:
+          </h1>
+
+          <InfiniteScroll
+            dataLength={infiniteScrollItems.length}
+            next={fetchMoreData}
+            hasMore={hasMore}
+            loader={undefined}
+          >
+            <section className="flex w-full flex-wrap justify-evenly">
+              {infiniteScrollItems ? (
+                infiniteScrollItems.map((item: Repo, index: number) => (
+                  <CustomCard program={true} key={index} item={item} />
+                ))
+              ) : (
+                <p>Loading...</p>
+              )}
+            </section>
+          </InfiniteScroll>
         </>
-    );
+      ) : (
+        <section className="flex w-full flex-wrap justify-evenly">
+          {searchResultsData.length ? (
+            searchResultsData.map((item, index) => (
+              <CustomCard program={true} key={index} item={item} />
+            ))
+          ) : (
+            <h1>Can&apos;t find what you are looking for</h1>
+          )}
+        </section>
+      )}
+    </>
+  );
 }
 
 const items = [...bergdb, ...maindb];
@@ -213,28 +213,28 @@ const items = [...bergdb, ...maindb];
 //      Exports getStaticProps for the Programs page.
 // =======================================================
 export async function getStaticProps() {
-    // --------- Most used Repos -----------
-    const mostUsed = items
-        .slice()
-        .sort((a, b) => b.stargazers_count - a.stargazers_count)
-        .slice(0, 10);
+  // --------- Most used Repos -----------
+  const mostUsed = items
+    .slice()
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .slice(0, 10);
 
-    // -------- Sort latest repos ----------
-    const sortedRepos = items
-        .slice()
-        .sort(
-            (a, b) =>
-                new Date(b.created_at).getTime() - new Date(a.created_at).getTime(),
-        );
+  // -------- Sort latest repos ----------
+  const sortedRepos = items
+    .slice()
+    .sort(
+      (a, b) =>
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime(),
+    );
 
-    // ----------- Latest Repos ------------
-    const top10LatestRepos = sortedRepos.slice(0, 10);
+  // ----------- Latest Repos ------------
+  const top10LatestRepos = sortedRepos.slice(0, 10);
 
-    // ------- Return Repos as Props -------
-    return {
-        props: {
-            mostUsed,
-            top10LatestRepos,
-        },
-    };
+  // ------- Return Repos as Props -------
+  return {
+    props: {
+      mostUsed,
+      top10LatestRepos,
+    },
+  };
 }
