4cb460d4ae717184e9110bf4e46b5fdf983d5b11
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-11-21 01:57:51 +0530

    ok

--- scripts/createDeepSearchDB.zig
+++ scripts/createDeepSearchDB.zig
@@ -16,29 +16,40 @@ pub fn main() !void {
     const all_items = json.value.array.items;
     helperFunctions.print("{{", .{});
     for (all_items, 0..) |item, i| {
-        std.debug.print("{}\n", .{i});
-        const full_name = item.object.get("full_name").?.string;
-        helperFunctions.print("\"{s}\"", .{full_name});
+        const full_name = try std.ascii.allocLowerString(allocator, item.object.get("full_name").?.string);
+        defer allocator.free(full_name);
+        const description = try std.ascii.allocLowerString(allocator, item.object.get("description").?.string);
+        defer allocator.free(description);
+        helperFunctions.print("\"{s}\":\"{s} ", .{ full_name, description });
+
         const url_to_fetch = try helperFunctions.concatenate(allocator, "https://raw.githubusercontent.com/", full_name, "/master/README.md");
         defer allocator.free(url_to_fetch);
+
         const result = try helperFunctions.fetchNormal(allocator, url_to_fetch);
         defer allocator.free(result);
-        const result_truncated = if (result.len > 7000) result[0..7000] else result;
-        helperFunctions.print(":\"", .{});
-        const description = item.object.get("description").?.string;
-        helperFunctions.print("{s} ", .{description});
+
         var previous_char: u8 = ' ';
+
+        const result_truncated = switch (result.len) {
+            0...7000 => result,
+            else => result[0..7000],
+        };
         for (result_truncated) |char| {
             // I have added heavy compression onto this to make sure I don't exceed the 1mb limit.
             // The following will also optimize the search results.
             // The search database will be purer.
             switch (char) {
-                ' ' => if (previous_char != ' ') helperFunctions.print(" ", .{}),
-                'a'...'z', 'A'...'Z', '0'...'9' => helperFunctions.print("{c}", .{std.ascii.toLower(char)}),
+                ' ', '\n' => if (previous_char != ' ' and previous_char != '\n') helperFunctions.print(" ", .{}),
+                'A'...'Z' => helperFunctions.print("{c}", .{char | 0b00100000}), // 0b00100000 magic number for lower caseing
+                'a'...'z', '0'...'9' => helperFunctions.print("{c}", .{char}),
                 else => {},
             }
             previous_char = char;
         }
-        if (i != all_items.len - 1) helperFunctions.print("\",", .{}) else helperFunctions.print("\"}}", .{});
+        if (all_items.len - 1 != i) {
+            helperFunctions.print("\",", .{});
+        } else {
+            helperFunctions.print("\"}}", .{});
+        }
     }
 }
