216bd55c1427db809994c38a786cf09f5dcdd10a
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-12-25 15:28:32 +0530

    Bringing old components to this app, need to modify these

---
+++ src/components/+LeftMiniTitle.svelte
@@ -0,0 +1,48 @@
+<script>
+	let props = $props();
+</script>
+
+<h1
+	class="titles my-5 ml-10 flex w-fit items-center rounded-2xl border-2 border-slate-400 px-4 pt-[18px] pb-4 text-left text-xl font-semibold shadow-lg shadow-black"
+>
+	<div class="flex translate-y-[-5px] items-center gap-3">
+		<div class="relative flex w-8">
+			<svg
+				stroke="currentColor"
+				fill="currentColor"
+				strokewidth="0"
+				viewBox="0 0 25 25"
+				height="25"
+				width="25"
+				class="absolute left-0 z-10 -translate-y-[6px]"
+				xmlns="http://www.w3.org/2000/svg"
+				strokeWidth="0"
+				><path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M8 18V6l8 6-8 6Z"
+				></path></svg
+			><svg
+				stroke="currentColor"
+				fill="currentColor"
+				strokewidth="0"
+				viewBox="0 0 25 25"
+				height="25"
+				width="25"
+				class="absolute left-3 -translate-y-[6px]"
+				xmlns="http://www.w3.org/2000/svg"
+				strokeWidth="0"
+				><path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M8 18V6l8 6-8 6Z"
+				></path></svg
+			>
+		</div>
+	</div>
+	<span>&nbsp;{props.name}</span>
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import LeftMiniTitle from '../components/+LeftMiniTitle.svelte';
+</script>
+
+<div class="flex flex-col items-center">
+	<div class="searchArea rounded-lg sm:m-5 sm:p-5 sm:shadow-lg sm:shadow-black">
+		<h1 class="my-5 text-center text-2xl font-semibold">Search Ziglang Packages</h1>
+		<div class="flex">
+			<div data-testid="flowbite-tooltip-target" class="w-fit">
+				<div class="ml-4 flex">
+					<div class="relative w-max max-w-36 min-w-10">
+						<label for="dropDownID" class="hidden">Filter</label><select
+							class="block w-full rounded-lg border border-yellow-500 bg-yellow-50 p-2.5 text-sm text-yellow-900 placeholder-yellow-700 focus:border-yellow-500 focus:ring-yellow-500 disabled:cursor-not-allowed disabled:opacity-50 dark:border-yellow-400 dark:bg-yellow-100 dark:focus:border-yellow-500 dark:focus:ring-yellow-500"
+							id="dropDownID"
+							><option>No Filter</option><option>api</option><option>http</option><option
+								>rest</option
+							><option>gamedev</option><option>gui</option><option>cross-platform</option></select
+						>
+					</div>
+				</div>
+			</div>
+			<div class="mx-4 mb-5 flex w-60 max-w-72">
+				<div class="relative w-full">
+					<input
+						class="block w-full rounded-lg border border-slate-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-600 dark:bg-[#2e2e2e] dark:text-white dark:placeholder-gray-400 dark:focus:border-cyan-500 dark:focus:ring-cyan-500"
+						type="text"
+						placeholder="Search 500+ Zig libraries"
+						autofocus=""
+						id="search_box"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div>
+<LeftMiniTitle name="Recently Released:" />
+</div>
