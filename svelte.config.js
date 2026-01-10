5f41ad26b4666aee1a50b4001f50c743369e94e0
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-01-11 02:38:58 +0530

    removed pre render

--- svelte.config.js
+++ svelte.config.js
@@ -12,9 +12,6 @@ const config = {
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter(),
-        prerender: {
-            default: true
-        }
     }
 };
 
