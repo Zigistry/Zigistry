1a9e163178efc37042e5ed1edbcf8e2539c9725a
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-01-11 17:53:42 +0530

    added nice frontend for packages default branch, in next commit i'll implement this for all the pages.

---
+++ src/components/+dependencyCard.svelte
@@ -0,0 +1,58 @@
+<script>
+    const data = $props();
+    const name = data.name;
+    const hash = data.hash;
+    const url = data.url;
+    const updated_at = data.updated_at;
+
+    // only get the repository url from this using regex
+    // using : https?://([a-zA-Z0-9.-]*/[^/]+/[^/]+)(?:/archive/|$)
+
+    const repoUrlMatch = url.match(
+        /https?:\/\/([a-zA-Z0-9.-]*\/[^/]+\/[^/]+)(?:\/archive\/|$)/
+    );
+    const repo_link = repoUrlMatch ? `https://${repoUrlMatch[1]}` : data.url;
+
+
+</script>
+
+
+<div
+    class="transform rounded-lg bg-gray-100 p-3 transition-all duration-300 hover:scale-102 hover:bg-gray-200 sm:p-4 dark:bg-[#2e2e2e] dark:hover:bg-slate-600"
+>
+    <div class="space-y-2 sm:space-y-3">
+        <div class="flex items-center space-x-2 text-sm sm:text-base">
+            <span class="font-medium text-emerald-600 dark:text-emerald-400">Name</span><span
+                class="text-black dark:text-white">${name}</span
+            >
+        </div>
+        <div class="space-y-1">
+            <div class="text-sm font-medium text-sky-600 sm:text-base dark:text-sky-400">URL</div>
+            <a
+                href={repo_link}
+                target="_blank"
+                class="text-xs break-all text-black hover:text-sky-500 sm:text-sm dark:text-white dark:hover:text-sky-300"
+                >{repo_link}</a
+            >
+        </div>
+        <div class="space-y-1">
+            <div class="text-sm font-medium text-purple-600 sm:text-base dark:text-purple-400">
+                Tag URL
+            </div>
+            <a
+                href={url}
+                target="_blank"
+                class="text-xs break-all text-black hover:text-purple-500 sm:text-sm dark:text-white dark:hover:text-purple-300"
+                >{url}</a
+            >
+        </div>
+        <div class="flex items-center space-x-2">
+            <span class="text-sm font-medium text-amber-600 sm:text-base dark:text-amber-400"
+                >Commit</span
+            ><code
+                class="relative truncate rounded bg-gray-200 px-2 py-1 font-mono text-xs text-gray-800 hover:absolute hover:z-10 hover:overflow-visible hover:whitespace-normal hover:shadow-lg sm:text-sm dark:bg-slate-800 dark:text-white"
+                >{hash}</code
+            >
+        </div>
+    </div>
+</div>
--- src/routes/packages/[provider]/[owner_name]/[repo_name]/+page.svelte
+++ src/routes/packages/[provider]/[owner_name]/[repo_name]/+page.svelte
@@ -1,10 +1,18 @@
 <script>
     import Card from '../../../../../components/+card.svelte';
+    import TimeAgo from 'javascript-time-ago';
+    import en from 'javascript-time-ago/locale/en';
+    import { Badge } from 'flowbite-svelte';
+
+    TimeAgo.addDefaultLocale(en);
+    import DependencyCard from '../../../../../components/+dependencyCard.svelte';
     import { Tabs, TabItem } from 'flowbite-svelte';
     import DOMPurify from 'dompurify';
     import { load } from './+page.js';
     import { marked } from 'marked';
     import { onMount } from 'svelte';
+    import LeftMiniTitle from '../../../../../components/+LeftMiniTitle.svelte';
+    import { InfoIcon } from '@lucide/svelte';
     const { data } = $props();
     const key = data.complete_correct_name;
     const value = data.value;
@@ -34,103 +42,143 @@
             });
     }
 
+    const timeAgo = new TimeAgo('en-US');
+
     onMount(() => {
         load_readme();
     });
-
 </script>
 
-{#if provider_id === 'gh'}
-    <Card
-        avatar_url={'https://avatars.githubusercontent.com/' + name_splitted[1]}
-        owner_name={name_splitted[1]}
-        repo_name={name_splitted[2]}
-        stars={library.s}
-        description={library.d}
-        watchers={library.w}
-        forks={library.f}
-        issues={library.i}
-        provider="gh"
-        spdx_id={library.l}
-        minimum_zig_version={library.dbi.m}
-        type_of_card="special-display"
-    />
-{:else if provider_id === 'cb'}
-    <Card
-        avatar_url={'https://codeberg.org/avatars/' + library.a}
-        owner_name={name_splitted[1]}
-        repo_name={name_splitted[2]}
-        description={library.d}
-        watchers={library.w}
-        stars={library.s}
-        forks={library.f}
-        issues={library.i}
-        minimum_zig_version={library.dbi.m}
-        provider="cb"
-        spdx_id={library.l}
-        type_of_card="special-display"
-    />
+<LeftMiniTitle name={library.db + ' branch details'} icon={InfoIcon} />
+
+<Badge rounded color="blue" class="mb-0 mt-2 ml-10 mr-0 text-xl"
+    >Last updated: {timeAgo.format(new Date(library.p))}</Badge
+>
+
+{#if library.s != 0}
+<Badge rounded color="indigo" class="mb-0 mt-2 ml-10 text-xl"
+    >Size: {library.s} KB</Badge
+>
 {/if}
 
-<h1>Currently viewing: HEAD Branch</h1>
+<div class="mt-3 min-h-screen p-2 sm:px-4 sm:pb-4 md:px-6 md:pb-6 lg:px-8 lg:pb-8">
+    {#if provider_id === 'gh'}
+        <Card
+            avatar_url={'https://avatars.githubusercontent.com/' + name_splitted[1]}
+            owner_name={name_splitted[1]}
+            repo_name={name_splitted[2]}
+            stars={library.s}
+            description={library.d}
+            watchers={library.w}
+            forks={library.f}
+            issues={library.i}
+            provider="gh"
+            spdx_id={library.l}
+            minimum_zig_version={library.dbi.m}
+            type_of_card="special-display"
+        />
+    {:else if provider_id === 'cb'}
+        <Card
+            avatar_url={'https://codeberg.org/avatars/' + library.a}
+            owner_name={name_splitted[1]}
+            repo_name={name_splitted[2]}
+            description={library.d}
+            watchers={library.w}
+            stars={library.s}
+            forks={library.f}
+            issues={library.i}
+            minimum_zig_version={library.dbi.m}
+            provider="cb"
+            spdx_id={library.l}
+            type_of_card="special-display"
+        />
+    {/if}
 
-<Tabs>
-    <TabItem open title="Readme">
-        <p class="text-sm text-gray-500 dark:text-gray-400">
-            {@html readme_content}
-        </p>
-    </TabItem>
-    <TabItem title="Dependencies">
-        <div class="text-sm text-gray-500 dark:text-gray-400">
-            {#if library.dbi.d.length === 0}
-                This package has no dependencies.
-            {:else}
-                <ul class="list-inside list-disc">
-                    {#each library.dbi.d as dependency}
+    <div class="mb-10"></div>
+
+    <Tabs tabStyle="full" class="mt-4 dark:bg-[#1e1e1e]">
+        <TabItem open title="Readme" class="w-full">
+            <div
+                class="rounded-lg bg-white p-3 shadow-lg shadow-black sm:rounded-lg sm:p-6 dark:bg-[#1e1e1e]"
+            >
+                <h2
+                    class="mb-3 border-b border-gray-300 pb-2 text-xl font-bold text-black sm:mb-4 sm:text-2xl dark:border-yellow-500 dark:text-white"
+                >
+                    README
+                </h2>
+                {@html readme_content}
+            </div>
+        </TabItem>
+        <TabItem title={"Dependencies: " + library.dbi.d.length} class="w-full">
+            <div
+                class="rounded-lg bg-white p-3 shadow-lg shadow-black sm:rounded-xl sm:p-6 dark:bg-[#1e1e1e]"
+            >
+                <div class="grid grid-cols-1 gap-3 sm:gap-4 md:grid-cols-2 lg:grid-cols-3">
+                    {#if library.dbi.d.length === 0}
+                        This package has no dependencies.
+                    {:else}
+                        {#each library.dbi.d as dependency}
+                            <DependencyCard
+                                name={dependency.n}
+                                url={dependency.u}
+                                hash={dependency.h}
+                            />
+                        {/each}
+                    {/if}
+                </div>
+            </div>
+        </TabItem>
+        <TabItem title={"Dependents: " + library.dts.length} class="w-full">
+            <div class="text-sm text-gray-500 dark:text-gray-400">
+                {#if library.dts.length === 0}
+                    This package has no Dependents.
+                {:else}
+                    <ul class="list-inside list-disc">
+                        {#each library.dts as dependent}
+                            <div class="p-4">
+                                <li>{dependent}</li>
+                            </div>
+                        {/each}
+                    </ul>
+                {/if}
+            </div>
+        </TabItem>
+        <TabItem title={"Versions: " + (library.r.length ? library.r.length : 0)} class="w-full">
+            <div class="text-sm text-gray-500 dark:text-gray-400">
+                {#if library_r.length === 0}
+                    This package has no version.
+                {:else}
+                    <ul class="list-inside list-disc">
                         <div class="p-4">
-                            <li>{dependency.n}</li>
-                            <li>{dependency.h}</li>
-                            <li>{dependency.u}</li>
-                        </div>
-                    {/each}
-                </ul>
-            {/if}
-        </div>
-    </TabItem>
-    <TabItem title="Dependents">
-        <div class="text-sm text-gray-500 dark:text-gray-400">
-            {#if library.dts.length === 0}
-                This package has no Dependents.
-            {:else}
-                <ul class="list-inside list-disc">
-                    {#each library.dts as dependent}
-                        <div class="p-4">
-                            <li>{dependent}</li>
-                        </div>  
-                    {/each}
-                </ul>
-            {/if}
-        </div>
-    </TabItem>
-    <TabItem title="Versions">
-        <div class="text-sm text-gray-500 dark:text-gray-400">
-            {#if library_r.length === 0}
-                This package has no version.
-            {:else}
-                <ul class="list-inside list-disc">
-                    <div class="p-4">
-                          <li><a class="underline" href="/packages/{provider_id === 'gh' ? 'github' : 'codeberg'}/{name_splitted[1]}/{name_splitted[2]}">Head Branch</a></li>
+                            <li>
+                                <a
+                                    class="underline"
+                                    href="/packages/{provider_id === 'gh'
+                                        ? 'github'
+                                        : 'codeberg'}/{name_splitted[1]}/{name_splitted[2]}"
+                                    >Head Branch</a
+                                >
+                            </li>
                             <li>Updated at: {new Date(library.p)}</li>
-                        </div>  
-                    {#each library_r as [a, b]}
-                        <div class="p-4">
-                          <li><a class="underline" href="/packages/{provider_id === 'gh' ? 'github' : 'codeberg'}/{name_splitted[1]}/{name_splitted[2]}/versions/{a}">{a}</a></li>
+                        </div>
+                        {#each library_r as [a, b]}
+                            <div class="p-4">
+                                <li>
+                                    <a
+                                        class="underline"
+                                        href="/packages/{provider_id === 'gh'
+                                            ? 'github'
+                                            : 'codeberg'}/{name_splitted[1]}/{name_splitted[2]}/versions/{a}"
+                                        >{a}</a
+                                    >
+                                </li>
 
-                            <li>Updated at: {new Date(b.p)}</li>
-                        </div>  
-                    {/each}
-                </ul>
-            {/if}
-        </div>
-    </TabItem>
-</Tabs>
+                                <li>Updated at: {new Date(b.p)}</li>
+                            </div>
+                        {/each}
+                    </ul>
+                {/if}
+            </div>
+        </TabItem>
+    </Tabs>
+</div>
