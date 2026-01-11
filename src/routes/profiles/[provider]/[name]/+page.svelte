fbad5ab0ae415a5130ce06796398ae65dc79ef51
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-01-12 01:56:05 +0530

    fixed profiles

--- src/routes/profiles/[provider]/[name]/+page.svelte
+++ src/routes/profiles/[provider]/[name]/+page.svelte
@@ -1,49 +1,39 @@
 <script>
     import { SquareArrowOutUpRight, Package, Code } from '@lucide/svelte';
     const { data } = $props();
-    import database from '../../../../database.json';
     import LeftMiniTitle from '../../../../components/+LeftMiniTitle.svelte';
     import Card from '../../../../components/+card.svelte';
-    let packages = Object.entries(database.packages);
-    let programs = Object.entries(database.programs);
-    
+
+    const {
+        owner_name,
+        owner_details,
+        packages_from_owner,
+        programs_from_owner,
+        provider_id,
+        avatar_url,
+        owner_provider_link
+    } = data;
 </script>
 
 <div class="px-40 py-20 sm:px-20 sm:py-10">
-    {#if data.provider === 'github'}
-        <img
-            class="max-w-[150px] min-w-[100px] rounded-full"
-            src={'https://avatars.githubusercontent.com/' + data.owner_name}
-        />
-    {:else if data.provider === 'codeberg'}
-        <img
-            class="max-w-[150px] min-w-[100px] rounded-full"
-            src={'https://codeberg.org/avatars/' + data.user?.a}
-        />
-    {/if}
-    <h2 class="mt-4 font-bold">Name: {data.owner_name}</h2>
-    <p>Bio: {data.user?.b}</p>
-    {#if data.provider === 'github'}
-        <a
-            class="mt-2 flex w-fit items-center justify-center rounded-full bg-slate-800 p-2 hover:underline"
-            target="_blank"
-            href={'https://github.com/' + data.owner_name}
-            >GitHub&nbsp;<SquareArrowOutUpRight size={18} /></a
-        >
-    {:else if data.provider === 'codeberg'}
-        <a target="_blank" href={'https://codeberg.org/' + data.owner_name}
-            >Codeberg<SquareArrowOutUpRight /></a
-        >
-    {/if}
+    <img alt="profile" class="max-w-37.5 min-w-25 rounded-full" src={avatar_url} />
+    <h2 class="mt-4 font-bold">Name: {owner_name}</h2>
+    <p>Bio: {owner_details?.b}</p>
+    <a
+        class="mt-2 flex w-fit items-center justify-center rounded-full bg-slate-800 p-4 hover:underline"
+        target="_blank"
+        href={owner_provider_link}
+        >View profile at {provider_id === 'gh' ? 'GitHub' : 'Codeberg'}&nbsp;<SquareArrowOutUpRight
+            size={18}
+        /></a
+    >
 </div>
 <div>
-    <LeftMiniTitle icon={Package} name="Packages"></LeftMiniTitle>
-    <section class="flex w-full flex-wrap justify-evenly">
-        {#each packages as [name, library]}
-            {@const name_splitted = name.split('/')}
-            {@const name2 = name_splitted[0] + '/' + name_splitted[1]}
-            {@const sub = data.provider === 'github' ? 'gh' : 'cb'}
-            {#if name2 === sub + '/' + data.owner_name && sub === 'gh'}
+    {#if packages_from_owner.length > 0}
+        <LeftMiniTitle icon={Package} name="Packages" />
+        <section class="flex w-full flex-wrap justify-evenly">
+            {#each packages_from_owner as [name, library]}
+                {@const name_splitted = name.split('/')}
                 <Card
                     avatar_url={library.a}
                     owner_name={name_splitted[1]}
@@ -53,21 +43,19 @@
                     watchers={library.w}
                     forks={library.f}
                     issues={library.i}
-                    provider="gh"
+                    provider={provider_id}
                     spdx_id={library.l}
                     minimum_zig_version={library.dbi.m}
                     type_of_card="packages-display"
                 />
-            {/if}
-        {/each}
-    </section>
-    <LeftMiniTitle icon={Code} name="Programs"></LeftMiniTitle>
-    <section class="flex w-full flex-wrap justify-evenly">
-        {#each programs as [name, library]}
-            {@const name_splitted = name.split('/')}
-            {@const name2 = name_splitted[0] + '/' + name_splitted[1]}
-            {@const sub = data.provider === 'github' ? 'gh' : 'cb'}
-            {#if name2 === sub + '/' + data.owner_name && sub === 'gh'}
+            {/each}
+        </section>
+    {/if}
+    {#if programs_from_owner.length > 0}
+        <LeftMiniTitle icon={Code} name="Programs"></LeftMiniTitle>
+        <section class="flex w-full flex-wrap justify-evenly">
+            {#each programs_from_owner as [name, library]}
+                {@const name_splitted = name.split('/')}
                 <Card
                     avatar_url={library.a}
                     owner_name={name_splitted[1]}
@@ -77,12 +65,12 @@
                     watchers={library.w}
                     forks={library.f}
                     issues={library.i}
-                    provider="gh"
+                    provider={provider_id}
                     spdx_id={library.l}
                     minimum_zig_version={library.dbi.m}
                     type_of_card="program-display"
                 />
-            {/if}
-        {/each}
-    </section>
+            {/each}
+        </section>
+    {/if}
 </div>
