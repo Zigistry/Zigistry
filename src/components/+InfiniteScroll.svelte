6dfbb735c1659642c9bfcc61dce6b8b00c5441ba
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-01-26 18:58:04 +0530

    Infinite scroll is working with the new api

--- src/components/+InfiniteScroll.svelte
+++ src/components/+InfiniteScroll.svelte
@@ -12,10 +12,10 @@
     async function loadMore() {
         const data = await fetch(
             `http://localhost:8000/${all_props.thingy}/scroll?` +
-                encodeURI('per_page=20&page=' + page)
+                encodeURI('per_page=20&ge=' + page)
         );
         const data_res = await data.json();
-        items = [...items, ...Object.entries(data_res)];
+        items = [...items, ...data_res];
         page++;
     }
 
@@ -38,41 +38,20 @@
 <section class="flex w-full flex-wrap justify-evenly">
     {@html '<!--What!!!! package is a reserved keyword!!!!!!-->'}
     {#each items as library}
-        {#if name_splitted[0] === 'gh'}
-            <Card
-                avatar_url={library.a}
-                owner_name={name_splitted[1]}
-                repo_name={name_splitted[2]}
-                stars={library.s}
-                description={library.d}
-                watchers={library.w}
-                forks={library.f}
-                issues={library.i}
-                provider="gh"
-                spdx_id={library.l}
-                minimum_zig_version={library.dbi.m}
-                type_of_card={all_props.thingy === 'programs'
-                    ? 'program-display'
-                    : 'packages-display'}
-            />
-        {:else if name_splitted[0] === 'cb'}
-            <Card
-                avatar_url={library.a}
-                owner_name={name_splitted[1]}
-                repo_name={name_splitted[2]}
-                description={library.d}
-                watchers={library.w}
-                stars={library.s}
-                forks={library.f}
-                issues={library.i}
-                minimum_zig_version={library.dbi.m}
-                provider="cb"
-                spdx_id={library.l}
-                type_of_card={all_props.thingy === 'programs'
-                    ? 'program-display'
-                    : 'packages-display'}
-            />
-        {/if}
+        <Card
+            avatar_url={library.avatar_url}
+            owner_name={library.owner_name.split('/')[1] || library.owner_name}
+            repo_name={library.repo_name}
+            stars={library.stargazer_count}
+            description={library.description}
+            watchers={library.watchers_count}
+            forks={library.fork_count}
+            issues={library.issues_count}
+            provider={library.provider}
+            spdx_id={library.license}
+            minimum_zig_version={library.minimum_zig_version}
+            type_of_card={all_props.thingy === 'programs' ? 'program-display' : 'packages-display'}
+        />
     {/each}
 </section>
 <ul bind:this={listElement}></ul>
