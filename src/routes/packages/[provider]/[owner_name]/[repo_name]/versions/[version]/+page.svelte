2df7d921acd9d0afd11d85a03f8c2bd004be45a6
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-01-29 12:09:21 +0530

    removed setup.js also added if else to handle problems with loading, again, fixed file formattings as well.

--- src/routes/packages/[provider]/[owner_name]/[repo_name]/versions/[version]/+page.svelte
+++ src/routes/packages/[provider]/[owner_name]/[repo_name]/versions/[version]/+page.svelte
@@ -1,40 +1,66 @@
-<script>
+<script lang="ts">
+    import { page } from '$app/state';
     import TimeAgo from 'javascript-time-ago';
     import en from 'javascript-time-ago/locale/en';
     import PackageProgramDisplay from '../../../../../../../components/+packageProgramDisplay.svelte';
 
     TimeAgo.addLocale(en);
-    const { data } = $props();
-    const provider_id = $derived(data.provider_id);
-    const version = $derived(data.version);
-    const library = $derived(data.value);
-    const platform = provider_id === 'gh' ? 'GitHub' : 'Codeberg';
+
+    let library = $state(null);
+    let provider_id = $derived(page.params.provider === 'github' ? 'gh' : 'cb');
+    let provider = $derived(page.params.provider === 'github' ? 'GitHub' : 'Codeberg');
+    let version = $derived(page.params.version);
+
+    $effect(() => {
+        const complete_correct_name =
+            `${provider_id}/${page.params.owner_name}/${page.params.repo_name}`.toLowerCase();
+        fetch(
+            `https://rohanvashisht-zigistrybackend.hf.space/packages?q=${complete_correct_name}&version=${version}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                library = data;
+            });
+    });
+
+    let title = $derived(
+        library
+            ? `Zig package: ${library.owner_name}/${library.repo_name} from ${provider} | Version: ${version}`
+            : 'Loading package version...'
+    );
+    let meta_description = $derived(
+        library ? 'Zig package: ' + library.description : 'Loading package details...'
+    );
 </script>
 
 <svelte:head>
-    <title
-        >Zig package: {library.owner_name}/{library.repo_name} from {platform} | Version: {version}</title
-    >
-    <meta name="description" content={'Zig package: ' + library.description} />
+    <title>{title}</title>
+    <meta name="description" content={meta_description} />
 </svelte:head>
 
-<PackageProgramDisplay
-    show_dependents={true}
-    {provider_id}
-    readme_url={library.readme_url}
-    version_name={version + ' version'}
-    releases={library.releases}
-    publish_date={library.published_at}
-    owner_name={library.owner_name}
-    repo_name={library.repo_name}
-    avatar_id={library.avatar_id}
-    stars_count={library.stars_count}
-    description={library.description}
-    forks_count={library.forks_count}
-    issues_count={library.issues_count}
-    license={library.license}
-    minimum_zig_version={library.minimum_zig_version}
-    published_date={library.published_at}
-    dependents={library.dependents}
-    dependencies={library.dependencies}
-/>
+{#if library}
+    <PackageProgramDisplay
+        show_dependents={true}
+        {provider_id}
+        readme_url={library.readme_url}
+        version_name={version + ' version'}
+        releases={library.releases}
+        publish_date={library.published_at}
+        owner_name={library.owner_name}
+        repo_name={library.repo_name}
+        avatar_id={library.avatar_id}
+        stars_count={library.stars_count}
+        description={library.description}
+        forks_count={library.forks_count}
+        issues_count={library.issues_count}
+        license={library.license}
+        minimum_zig_version={library.minimum_zig_version}
+        published_date={library.published_at}
+        dependents={library.dependents}
+        dependencies={library.dependencies}
+    />
+{:else}
+    <div class="flex h-screen items-center justify-center">
+        <div class="text-2xl font-bold text-black dark:text-white">Loading package version...</div>
+    </div>
+{/if}
--- src/routes/programs/[provider]/[owner_name]/[repo_name]/+page.svelte
+++ src/routes/programs/[provider]/[owner_name]/[repo_name]/+page.svelte
@@ -1,39 +1,63 @@
-<script>
+<script lang="ts">
+    import { page } from '$app/state';
     import TimeAgo from 'javascript-time-ago';
     import en from 'javascript-time-ago/locale/en';
     import PackageProgramDisplay from '../../../../../components/+packageProgramDisplay.svelte';
 
     TimeAgo.addLocale(en);
-    const { data } = $props();
-    const provider_id = $derived(data.provider_id);
-    const library = $derived(data.value);
-    const provider = provider_id === 'gh' ? 'GitHub' : 'Codeberg';
+
+    let library = $state(null);
+    let provider_id = $derived(page.params.provider === 'github' ? 'gh' : 'cb');
+    let provider = $derived(page.params.provider === 'github' ? 'GitHub' : 'Codeberg');
+
+    $effect(() => {
+        const complete_correct_name =
+            `${provider_id}/${page.params.owner_name}/${page.params.repo_name}`.toLowerCase();
+        fetch(`https://rohanvashisht-zigistrybackend.hf.space/programs?q=${complete_correct_name}`)
+            .then((res) => res.json())
+            .then((data) => {
+                library = data;
+            });
+    });
+
+    let title = $derived(
+        library
+            ? `Zig program: ${library.owner_name}/${library.repo_name} from ${provider} | Branch: ${library.default_branch_name}`
+            : 'Loading program...'
+    );
+    let meta_description = $derived(
+        library ? 'Zig program: ' + library.description : 'Loading program details...'
+    );
 </script>
 
 <svelte:head>
-    <title
-        >Zig program: {library.owner_name}/{library.repo_name} from {provider} | Branch: {library.default_branch_name}</title
-    >
-    <meta name="description" content={'Zig program: ' + library.description} />
+    <title>{title}</title>
+    <meta name="description" content={meta_description} />
 </svelte:head>
 
-<PackageProgramDisplay
-    show_dependents={false}
-    {provider_id}
-    readme_url={library.readme_url}
-    version_name={library.default_branch_name + ' branch'}
-    releases={library.releases}
-    publish_date={library.pushed_at}
-    owner_name={library.owner_name}
-    repo_name={library.repo_name}
-    avatar_id={library.avatar_id}
-    stars_count={library.stars_count}
-    description={library.description}
-    forks_count={library.forks_count}
-    issues_count={library.issues_count}
-    license={library.license}
-    minimum_zig_version={library.minimum_zig_version}
-    published_date={library.pushed_at}
-    dependents={[]}
-    dependencies={library.dependencies}
-/>
+{#if library}
+    <PackageProgramDisplay
+        show_dependents={false}
+        {provider_id}
+        readme_url={library.readme_url}
+        version_name={library.default_branch_name + ' branch'}
+        releases={library.releases}
+        publish_date={library.pushed_at}
+        owner_name={library.owner_name}
+        repo_name={library.repo_name}
+        avatar_id={library.avatar_id}
+        stars_count={library.stars_count}
+        description={library.description}
+        forks_count={library.forks_count}
+        issues_count={library.issues_count}
+        license={library.license}
+        minimum_zig_version={library.minimum_zig_version}
+        published_date={library.pushed_at}
+        dependents={[]}
+        dependencies={library.dependencies}
+    />
+{:else}
+    <div class="flex h-screen items-center justify-center">
+        <div class="text-2xl font-bold text-black dark:text-white">Loading program details...</div>
+    </div>
+{/if}
