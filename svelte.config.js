72561252e7d069a0718fbcd054bf5f6b2de465c9
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-05 23:40:59 +0530

    Added pre-rendering

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
     preprocess: vitePreprocess(),
 
     kit: {
-        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        prerender: {
+            entries: ['*']
+        },
+
         adapter: adapter({
             pages: './cloudflare/build',
             assets: './cloudflare/build',
