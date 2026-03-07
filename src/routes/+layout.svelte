8a09e83b6f15ffbec909afc1275262c2a67ea50d
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-08 01:57:29 +0530

    Added a nice graph route.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -99,6 +99,13 @@
                         >Programs</a
                     >
                 </li>
+                <li>
+                    <a
+                        href="/graph"
+                        class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                        >Graph</a
+                    >
+                </li>
                 <div
                     class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow dark:divide-gray-600 dark:bg-gray-700"
                     id="dropdownNavbar"
---
+++ src/routes/graph/+page.svelte
@@ -0,0 +1,18 @@
+<div class="graph-frame">
+    <iframe title="Zigistry dependency graph" src="/graph.html"></iframe>
+</div>
+
+<style>
+    .graph-frame {
+        width: 100%;
+        height: 80vh;
+        background-color: #1e1e1e;
+    }
+
+    .graph-frame iframe {
+        display: block;
+        width: 100%;
+        height: 100%;
+        border: 0;
+    }
+</style>
