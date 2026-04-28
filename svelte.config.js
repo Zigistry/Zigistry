60713a974fefc77413675e5f7045b67339082938
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-04-28 20:34:39 +0530

    Added enhanced security using csp and xframeoptions

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,23 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
+        csp: {
+            mode: 'hash',
+            directives: {
+                'default-src': ['self'],
+                'script-src': ['self'],
+                'style-src': ['self', 'unsafe-inline'],
+                'img-src': ['self', 'data:', 'https://avatars.githubusercontent.com', 'https://codeberg.org'],
+                'connect-src': ['self', 'https://rohanvashisht-zigistrybackend.hf.space', 'https://api.github.com', 'http://localhost:*', 'ws://localhost:*'],
+                'font-src': ['self'],
+                'object-src': ['none'],
+                'base-uri': ['self'],
+                'form-action': ['self'],
+                'frame-ancestors': ['none'],
+                'frame-src': ['self', https://www.youtube.com', 'https://www.youtube-nocookie.com'],
+                    'upgrade-insecure-requests': true
+            }
+        },
         prerender: {
             entries: ['/', '/programs', '/graph', '/about', '/help'],
             handleHttpError: 'warn',
